29c2a802269b048fe297ddc5e50a23a2d31bfdf0
Steven Ortiz <sortiz1726@users.noreply.github.com>
2021-10-01 01:45:32 +0000

    updates

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -4,22 +4,22 @@ exports.toRoman = function(num)
     
     const romanToArabic = 
     {
-        M : 1000,
+        'M' : 1000,
         
-        CM : 900,
-        D : 500,
-        CD : 400,
-        C : 100,
+        'CM' : 900,
+        'D' : 500,
+        'CD' : 400,
+        'C' : 100,
         
-        XC : 90,
-        L : 50,
-        XL : 40,
-        X : 10,
+        'XC' : 90,
+        'L' : 50,
+        'XL' : 40,
+        'X' : 10,
         
-        IX : 9,
-        V : 5,
-        IV : 4,
-        I : 1,   
+        'IX' : 9,
+        'V' : 5,
+        'IV' : 4,
+        'I' : 1,   
     }
     
     for(const roman in romanToArabic)
